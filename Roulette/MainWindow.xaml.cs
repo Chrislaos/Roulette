5ee345e9052314e622ae4d2376b688227128ddbd
Chrislaos <Chris_laos@hotmail.com>
2014-06-14 01:02:07 +0200

    Change
    Enum, delegate, thread

---
+++ Roulette/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace Roulette
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        rollBall rollingBall1 = new rollBall();
+        Thread T;
+        delegate void del();
+        del updateDelegate;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void rollButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //T = new Thread(process);
+                //T.Start();
+                process();
+            }
+            catch(Exception b)
+            {
+                statusBlock.Text = b.ToString();
+            }
+        }
+        public void process()
+        {
+            rollingBall1.randomNumber();
+            numberDisplay.Text = rollingBall1.rollPresent.ToString();
+
+            updateDelegate = () => switchVisibleColor(rollingBall1.ellipseColorDecider());
+            this.Dispatcher.Invoke(updateDelegate, new object[] { });
+
+            winningOrLosingDisplay(rollingBall1.checkIfWon(radioRed, radioBlack));
+            checkIfChoosen(rollingBall1.checkIfChecked(radioRed, radioBlack));
+        }
+        public void switchVisibleColor(Color tempColor)
+        {
+            ellipseColor.Fill = new SolidColorBrush(tempColor);
+        }
+        public void winningOrLosingDisplay(bool won)
+        {
+            if (won)
+                statusBlock.Text = "YOU WON";
+            else
+                statusBlock.Text = "YOU LOSE";
+        }
+        public void checkIfChoosen(bool checker)
+        {
+            if (checker) { }
+                
+            else { statusBlock.Text += ", You need to pick a side!"; }
+        }
+    }
+}
---
+++ Roulette/rollBall.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace Roulette
+{
+    class rollBall
+    {
+        public enum states{ RED, BLACK, NONE };
+        public enum winLose{ WIN, LOSE };
+        public int[] redNumbers = new int[] {1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36 };
+        public int[] blackNumbers = new int[] { 2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35 };
+        bool checkColor = false;
+        public int rollPresent;
+
+        public states selectedState;
+
+        public void randomNumber()
+        {
+            Random rngNm = new Random();
+            rollPresent = rngNm.Next(1, 37);
+        }
+        public bool checkForColor(int tempInt, int[] tempIntArray)
+        {
+            checkColor = false;
+            foreach (int n in tempIntArray)
+            {
+                if (n == tempInt)
+                    checkColor = true;
+                else { }
+                    
+            }
+            return checkColor;
+        }
+        public Color ellipseColorDecider()
+        {
+            if (checkForColor(rollPresent, redNumbers)){
+                selectedState = states.RED;
+                return Colors.Red;
+            }
+            else if (checkForColor(rollPresent, blackNumbers)){
+                selectedState = states.BLACK;
+                return Colors.Black;
+            }
+            else{
+                selectedState = states.NONE;
+                return Colors.Transparent;
+            }
+        }
+        public bool checkIfWon(RadioButton r, RadioButton b)
+        {
+            if (r.IsChecked == true && selectedState == states.RED)
+                return true;
+            else if (b.IsChecked == true && selectedState == states.BLACK)
+                return true;
+            else
+                return false;
+        }
+        public bool checkIfChecked(RadioButton r, RadioButton b)
+        {
+            if (r.IsChecked == false && b.IsChecked == false)
+                return false;
+            else
+                return true;
+        }
+            
+        
+    }
+}
